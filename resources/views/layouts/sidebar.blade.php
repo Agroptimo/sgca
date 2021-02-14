6c2872b252e03063abb85cec4ef7bd3c41e29091
asomic <ti@auditcon.cl>
2021-02-14 02:48:07 -0300

    test

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -62,7 +62,7 @@
                     <span class="inline-flex justify-center items-center ml-4">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
                       </span>
-                  <span class="ml-2 text-sm tracking-wide truncate">{{Auth::user()->name}}</span>
+                  <span class="ml-2 text-sm tracking-wide truncate">Agroptimo</span>
                 </a>
               </li>
               <li>
